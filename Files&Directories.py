0213499c166f5344ac6650c8ed2f72dcfe75ad52
Florian Doumenc <floriand@google.com>
2020-10-27 13:28:34 -0400

    Files & Calendars

---
+++ Files&Directories.py
@@ -0,0 +1,29 @@
+#! python3
+# 14.1
+
+import os
+def getFilesCurrentDir():
+    print(os.listdir())
+
+getFilesCurrentDir()
+
+#14.2
+def getFilesParentDir():
+    print(os.listdir('..'))
+
+getFilesParentDir()
+
+
+#14.3
+def writeText(text, filename):
+    t = text
+    temp = open(filename + ".txt", "w")
+    temp.write(t)
+    temp.close
+
+writeText('This is a test of the emergency text system', 'test')
+
+#14.4
+file = open("test.txt")
+test2 = file.read()
+print(test2)
